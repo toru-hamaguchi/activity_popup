917705041ea590b1e090c6521dce10e8c29fd698
toru hamaguchi <hamaguchi@micro-wave.net>
2013-03-09 20:39:49 +0900

    Add activity status styles

--- scripts/views/activity.js
+++ scripts/views/activity.js
@@ -13,8 +13,33 @@
 			this.render();
 		},
 		render: function() {
-			this.$el.text(this.model.get('url'));
+			/* Listen status code changes. */
+			this.listenTo(this.model, 'change:statusCode', this.update);
+
+			/* Initialize view. */
+			this.update();
+
 			return this;
+		},
+
+		/**
+		 * Update.
+		 */
+		update: function() {
+			var url = this.model.get('url')
+				, statusCode = this.model.get('statusCode');
+
+			this.$el.text(url);
+
+			if (statusCode === undefined) {
+				this.$el.attr('class', 'status-none');
+			}
+			else if (statusCode === 200) {
+				this.$el.attr('class', 'status-200');
+			}
+			else {
+				this.$el.attr('class', 'status-error');
+			}
 		}
 	});
 
